05260c1be356f31236254fefcf1d74e789731b59
Daniel Karani <danielevans.karani@gmail.com>
2022-12-24 01:05:09 +0300

    Adds the User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
-
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, user_type, password=None):
+        if username is None:
+            raise TypeError('Users must have a username')
+        if email is None:
+            raise TypeError('Users must have a Email')
+
+        user = self.model(
+
+            username=username,
+            user_type=user_type,
+            email=self.normalize_email(email)
+
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, user_type, username, password=None):
+
+        user = self.create_user(
+
+            email,
+            user_type=user_type,
+            username=username,
+            password=password,
+
+        )
+
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+
+    USER_TYPE_CHOICES = (
+
+        ('employer', "Employer"),
+        ('supervisor', "Supervisor"),
+        ('student', "Student"),
+    )
+
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'user_type']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
