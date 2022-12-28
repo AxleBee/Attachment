4c437063c2f9e98d8c57b4738b5f2edc9cd60810
Daniel Karani <danielevans.karani@gmail.com>
2022-12-28 18:12:17 +0300

    Adds the logout and login api endpoints

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import *
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    
+
     path('register/', RegisterView.as_view(), name="register"),
-    path('login/', LoginAPIView.as_view(), name="login"),
+    path('login/', login_user, name="login"),
+    path('logout/', logout_user, name="logout"),
+    path('student/', StudentAPIView.as_view())
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
