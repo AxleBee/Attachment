017afb5e7cf46f94daa5bafd5908f7c686c64c1d
Daniel Karani <danielevans.karani@gmail.com>
2022-12-30 10:51:33 +0300

    Adds routes

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,10 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
 
-    path('register/', RegisterView.as_view(), name="register"),
-    path('login/', login_user, name="login"),
-    path('logout/', logout_user, name="logout"),
-    path('student/', StudentAPIView.as_view())
+  path('register/', register, name ='register'),
+  path('login/', login_view, name ='login'),
+  path('register/', logout_view, name ='logout')
 ]
 
 if settings.DEBUG:
