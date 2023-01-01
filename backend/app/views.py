9c88d46f6dfb33800f63f41ad28f49a2130a32b3
Daniel Karani <danielevans.karani@gmail.com>
2023-01-01 08:55:08 +0300

    Adds the add-attachment view

--- backend/app/views.py
+++ backend/app/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from .forms import *
 
+# # Authentication Views ##
+
 
 def register(request):
     form = CustomUserCreationForm()
@@ -24,27 +26,62 @@ def login_view(request):
         if current_user.user_type == "employer":
             pass
         if current_user.user_type == "student":
-            pass
+            return redirect('add_attachment')
     email = ""
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
         remember_me = request.POST.get('remember_me')
-        user = authenticate(request, email = email, password = password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, f'{user.username} logged in successfully.')
-        
+            messages.success(
+                request, f'{user.username} logged in successfully.')
+
         else:
-            messages.warning(request, 'password or email is incorrect.' )
+            messages.warning(request, 'password or email is incorrect.')
     return render(request, 'app/login.html', context={"email": email})
 
-
-
-    
-    
 
 def logout_view(request):
     print(request.user)
     logout(request)
     return redirect('login')
+
+
+# # Student Views ##
+def add_attachment(request):
+    if (request.user.last_login == request.user.date_joined):
+        messages.success(
+            request, f'update your profile detail')
+        return redirect('student_profile')
+
+    if request.method == 'POST':
+        attachment_name = request.POST.get('attachment')
+        organisation_name = request.POST.get('organisation')
+        start_date = request.POST.get("start")
+        end_date = request.POST.get('end')
+        attachment = Attachment.objects.create(
+
+            attachment_name=attachment_name,
+            organisation_name=organisation_name,
+            start_date=start_date,
+            end_date=end_date
+
+        )
+        attachment.student = request.user.student
+        attachment.save()
+    
+    return (request, 'app/attachment.html', locals())
+
+
+def add_profile(request):
+    pass
+
+
+def add_logbook(request):
+    pass
+
+
+def view_comments(request):
+    pass
