197f258e90ef89e23fe99c5502e602e99aadc380
Daniel Karani <danielevans.karani@gmail.com>
2023-01-30 20:18:32 +0300

    Deletes folder

--- front-end/src/components/Students/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Card, CardContent, CardHeader } from '@mui/material';
-
-export const StudentDashboard=() => (
-    <Card>
-        <CardHeader title="Welcome student to your dashboard" />
-        <CardContent>Congrats on your internship...</CardContent>
-    </Card>
-);
